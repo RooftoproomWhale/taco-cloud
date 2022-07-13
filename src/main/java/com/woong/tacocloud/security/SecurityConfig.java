fff1fc70460a02e10897c2f2dd09bd5a771f6a0c
RooftoproomWhale <gh8238@naver.com>
2022-07-13 17:17:24 +0900

    chapter 4 - Spring Security adapter 없이 적용 ~ing

---
+++ src/main/java/com/woong/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,89 @@
+package com.woong.tacocloud.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig
+{
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/design","/orders")
+                .hasRole("USER")
+                .antMatchers("/","/**").permitAll()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+    /*@Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.builder()
+                .username("user1")
+                .password("password1")
+                .passwordEncoder(p -> passwordEncoder().encode(p))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("user2")
+                .password("password2")
+                .passwordEncoder(p -> passwordEncoder().encode(p))
+                .roles("USER")
+                .build();
+
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(user2);
+        return users;
+    }*/
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("user2")
+                .password("password2")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, user2);
+    }*/
+}
